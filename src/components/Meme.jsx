31e195c298a5b6e0cfded0e3a013a2758c58eb10
Vinay Maurya <vinaymaurya105@gmail.com>
2022-05-03 23:16:00 +0530

    commit meme page

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,41 +1,73 @@
-// import React from "react";
-// import { Box, Button, TextField, makeStyles } from "@material-ui/core";
+import React, { useState } from "react";
+import { Box, Button, TextField, makeStyles } from "@material-ui/core";
 
-// const useStyle = makeStyles({
-//   inputBox: {
-//     display: "flex",
-//     alignItems: "center",
-//     padding: 30,
-//     justifyContent: "space-around",
-//   },
+const useStyle = makeStyles({
+  inputBox: {
+    display: "flex",
+    alignItems: "center",
+    padding: 20,
+    justifyContent: "space-around",
+    height: 100,
+  },
 
-//   input : {
+  input: {
+    padding: "100px  0",
+    width: 320,
+    border: "none",
+  },
 
-//   },
+  btn: {
+    color: "#fff",
+    background: "linear-gradient(90.41deg, #711F8D 1.14%, #A818DA 100%)",
+    textTransform: "unset",
+    borderRadius: 5,
+    width: "86%",
+    padding: 10,
+  },
+});
 
-//   btn: {
-//     color: "#fff",
-//     background: "linear-gradient(90.41deg, #711F8D 1.14%, #A818DA 100%)",
-//     textTransform: "unset",
-//     borderRadius: 5,
-//     width: "90%",
-//   },
-// });
+function Meme() {
+  const classes = useStyle();
 
-// function Meme() {
-//   const classes = useStyle();
-//   return (
-//     <>
-//       <Box className={classes.inputBox}>
-//         <TextField variant="outlined" className={classes.input} />
-//         <TextField variant="outlined" className={classes.input} />
-//       </Box>
+  const [data, setData] = useState("");
 
-//       <Button variant="contained" className={classes.btn}>
-//         Get a new meme image 🖼
-//       </Button>
-//     </>
-//   );
-// }
+  const handleChange = (e) => {
+    setData(e.target.value);
+  };
+  return (
+    <>
+      <Box className={classes.inputBox}>
+        <TextField
+          variant="outlined"
+          className={classes.input}
+          placeholder="Top text"
+          value=""
+          onChange={handleChange}
+          inputProps={{
+            style: {
+              height: "10px",
+            },
+          }}
+        />
+        <TextField
+          variant="outlined"
+          className={classes.input}
+          placeholder="bottom-text"
+          value=""
+          onChange={handleChange}
+          inputProps={{
+            style: {
+              height: "10px",
+            },
+          }}
+        />
+      </Box>
 
-// export default Meme;
+      <Button variant="contained" className={classes.btn}>
+        Get a new meme image 🖼
+      </Button>
+    </>
+  );
+}
+
+export default Meme;
